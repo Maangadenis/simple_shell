bbfe9a6c9d740152e26497e26e64fb31046f7d5c
Denis Harold Maanga <99499954+Maangadenis@users.noreply.github.com>
2022-05-19 13:37:15 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,63 +1,41 @@
-#include "shell.h"
-
-
-/**
- * main - entry point
- * Return: 1 or 0 if is succesfull or failed
- */
-
-int main(void)
-{
-char *buffer = NULL, **command = NULL, *path = NULL;
-int fd = 0, tty = FALSE, exit_code = 0, flag, status = 1;
-int *exit_status = &exit_code;
-struct stat st;
-
-signal(SIGINT, sig_handler);
-while (status)
-{
-if (isatty(fd) == TRUE)
-write(STDIN_FILENO, "$ ", 2);
-buffer = read_command();
-if (buffer == NULL || buffer[0] == '\n')
-continue;
-command = split_command(buffer);
-if (command[0] == NULL)
-{
-free(buffer);
-free(command);
-continue;
-}
-if (is_builtin(command[0]) == FALSE)
-{
-if (stat(command[0], &st) == 0)
-flag = TRUE;
-else
-{
-path = split_path(command[0]);
-flag = FALSE;
-}
-status = exc_argument(command, path, buffer, tty, flag);
-}
-else
-{
-status = exec_builtin(command, path, buffer, exit_status);
-clean_memory(command, path, buffer, TRUE);
-if (status == -2)
-break;
-}
-}
-return (exit_code);
-}
+#include "main.h"
 
 /**
- * sig_handler - catch the ctrl + c
- * @sig: the number of the signal
+ * main - prints a simple shell
+ * @ac: number of arguments
+ * @argv: arrays of arguments
+ *
+ * Return: Always 0.
  */
-
-void sig_handler(int sig)
+int main(int ac, char **argv)
 {
-(void)sig;
-signal(SIGINT, sig_handler);
-write(STDIN_FILENO, "\n$ ", 3);
+	char *prog_name = argv[0];
+	char *command;
+	size_t len = 100;
+	size_t command_char;
+
+	command = malloc(len * sizeof(char));
+
+	if (command == NULL)
+		exit(1);
+
+	while (1)
+	{
+		if (isatty(STDIN_FILENO))
+		{
+			char *prompt = "$ ";
+
+			write(STDOUT_FILENO, prompt, strlen(prompt) + 1);
+		}
+
+		if (getline(&command, &len, stdin) == -1)
+		{
+			if (isatty(STDIN_FILENO))
+				write(STDOUT_FILENO, "\n", 2);
+
+			free(command);
+		}
+	}
+
+	return (0);
 }
